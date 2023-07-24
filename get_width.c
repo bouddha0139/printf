b59afcb42430237d337949d57f8bd1be171c52aa
Allan Kevin <134234752+Aellun@users.noreply.github.com>
2023-07-24 22:53:24 +0300

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -6,7 +6,7 @@
  * @i: List of arguments to be printed.
  * @list: list of arguments.
  *
- * Return: width.
+ * Return: width. specification for formatting, 0 if no width found
  */
 int get_width(const char *format, int *i, va_list list)
 {
@@ -20,16 +20,17 @@ int get_width(const char *format, int *i, va_list list)
 			width *= 10;
 			width += format[curr_i] - '0';
 		}
+/*If the char is '*', read the width from the variable arguments (va_list)*/
 		else if (format[curr_i] == '*')
 		{
-			curr_i++;
+			curr_i++; /*Move to the next character after '*'*/
 			width = va_arg(list, int);
-			break;
+			break;/*Exit the loop since width is obtained*/
 		}
 		else
 			break;
 	}
-
+/*Update the pointer 'i' to point to the last checked position in the format string*/
 	*i = curr_i - 1;
 
 	return (width);
