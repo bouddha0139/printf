570971d0ced2b88d4d2c56368766bb15f819a2d2
Allan Kevin <134234752+Aellun@users.noreply.github.com>
2023-07-24 22:46:16 +0300

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -5,18 +5,24 @@
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
- * Return: Precision.
+ * Return: size specifiier: size to cast the argument,
+ *0 if no size specifier found
  */
 int get_size(const char *format, int *i)
 {
 	int curr_i = *i + 1;
 	int size = 0;
 
+/* check if the chr after current position is I(for long)*/
 	if (format[curr_i] == 'l')
 		size = S_LONG;
+
+/* check if the chr after current position is h (for short)*/
 	else if (format[curr_i] == 'h')
 		size = S_SHORT;
 
+/*If size is still 0, it means no size specifier was found*/
+/*otherwise, update the pointer 'i' to point to the next position*/
 	if (size == 0)
 		*i = curr_i - 1;
 	else
